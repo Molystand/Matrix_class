29ce5887e043f95497716c41a0e6d5811e058751
Molystand <molystand@gmail.com>
2017-08-02 22:04:53 +0300

    Базовая функциональность. Первый работающий прототип.

---
+++ Matrix.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix_class
+{
+    class Matrix
+    {
+        private double[, ] matrix;
+        private int row_;        // Количество строк
+        private int col_;        // Количество столбцов
+
+
+        public int Row
+        {
+            get
+            {
+                return row_;
+            }
+        }
+        public int Col
+        {
+            get
+            {
+                return col_;
+            }
+        }
+
+
+        public Matrix(int row, int col)
+        {
+            row_ = row;     /// Сменить именование
+            col_ = col;
+
+            matrix = new double[Row, Col];
+            /*
+            matrix = new double[Row][];
+            for (int i = 0; i < Row; i++)
+            {
+                matrix[i] = new double[Col];
+            }
+            */
+        }
+
+        // Ввод с клавиатуры
+        /*
+        public void Read()
+        {
+            for (int i = 0; i < Row; i++)
+            {
+                for (int j = 0; j < Col; j++)
+                {
+                    matrix[i, j] = double.Parse(Console.ReadLine());    /// Доделать, чтобы можно было вводить через пробел
+                }
+            }
+        }
+        */
+
+        public override string ToString()
+        {
+            string str = "";
+
+            for (int i = 0; i < Row; i++)
+            {
+                for (int j = 0; j < Col; j++)
+                {
+                    str += matrix[i, j].ToString() + ' ';
+                }
+                str += '\n';
+            }
+
+            return str;
+        }
+
+        public double this[int row, int col]
+        {
+            get
+            {
+                return matrix[row, col];
+            }
+            set
+            {
+                matrix[row, col] = value;
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,6 +10,13 @@ namespace Matrix_class
     {
         static void Main(string[] args)
         {
+            Matrix matr = new Matrix(2, 3);
+            matr[0, 1] = 7;
+            matr[1, 2] = 2;
+
+            Console.WriteLine("{0}", matr[1, 2]);
+            Console.WriteLine("Matrix:\n{0}", matr);
+            Console.ReadLine();
         }
     }
 }
